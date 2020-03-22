b7bb5c3b1bd8ef40857af2af95af3a5ea303dd17
Dima Kolyada <kolyadadmitri@gmail.com>
2020-03-22 18:06:09 +0300

    Begin1

---
+++ Begin1.cpp
@@ -0,0 +1,14 @@
+﻿
+//Дана сторона квадрата a. Найти его площадь S = a2.
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, S;
+    cout << "enter a";
+    cin >> a;
+    S = a * a;
+    cout << S;
+
+}
+
