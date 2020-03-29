25d0eed7471f63b4d61292061ab6e6701bd17700
Dima Kolyada <kolyadadmitri@gmail.com>
2020-03-29 19:36:28 +0300

    Begin2

---
+++ Task_Begin2.cpp
@@ -0,0 +1,18 @@
+﻿//Даны катеты прямоугольного треугольника a и b. Найти его гипотенузу c и периметр P
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    double c;
+    cout << "enter a"<<endl;
+    cin >> a;
+    cout << "enter b"<<endl;
+    cin >> b;
+    c = sqrt(a * a + b * b);
+    cout << "c=" << c<<endl;
+    cout << "P=" << a + b + c;
+}
+
